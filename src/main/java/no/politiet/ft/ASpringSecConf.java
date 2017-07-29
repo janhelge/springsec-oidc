df6bcfc89e5b326893805b3bb7b9c50d29e946d3
Jan Helge Salvesen <Jan.Helge.Salvesen@gmail.com>
2017-07-29 15:51:44 +0200

    Fungerende Basic innlogging, med debugging

--- src/main/java/no/politiet/ft/ASpringSecConf.java
+++ src/main/java/no/politiet/ft/ASpringSecConf.java
@@ -1,17 +1,27 @@
 package no.politiet.ft;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 @Configuration
 @EnableWebSecurity
 public class ASpringSecConf extends WebSecurityConfigurerAdapter{
 
+    private static final Logger log = LoggerFactory.getLogger(ASpringSecConf.class);
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+
+        log.debug("ConfigureGlobal was actially called");
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER")
@@ -20,34 +30,98 @@ public class ASpringSecConf extends WebSecurityConfigurerAdapter{
         ;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+////        http
+////                .authorizeRequests()
+////                .anyRequest().authenticated()
+////                .and()
+////                .formLogin()
+////                .loginPage("/login")
+////                .permitAll();
+//
+//
 //        http
 //                .authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
+//                .antMatchers("/secured/**").access("hasRole('USER')")
+//                //.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
+//                .and().formLogin()
+//                .loginPage("/login?reason=login")
+//                //.loginProcessingUrl("/perform_login")
+//                .failureUrl("/login?reason=failure")
+//                .defaultSuccessUrl("/secured/hei")
 //                .permitAll();
+//
+////        http
+////                .authorizeRequests()
+////                .antMatchers("/secured/**")
+////                .hasAnyRole("USER", "DISTRIKT_LISTE", "DISTRIKT")
+////                .anyRequest()
+////                .authenticated()
+////        ;
+//    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //Boolean isDebug = environment.getProperty("spring.security.debug", Boolean.class, Boolean.FALSE);
+        web.debug(Boolean.TRUE);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
 
         http
-                .authorizeRequests()
-                .antMatchers("/secured/**").access("hasRole('ROLE_USER')")
-                //.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
-                .and().formLogin()
-                .loginPage("/login?reason=login")
-                .failureUrl("/login?reason=failure")
-                .defaultSuccessUrl("/secured/hei")
-                .permitAll();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+
+// 		Vurdere om dette trengs etterhvert ...
+//		http
+//		.exceptionHandling()
+//		.accessDeniedPage("/access-denied.html");
+
+        http
+                .csrf()
+                .disable() // <== Trenger csrf().disable() for at "/log" (dvs logout) skal fungere
+        ;
+
+        http
+                .jee()
+                //.mappableRoles("LAND", "DISTRIKT_LISTE", "DISTRIKT")
+                .mappableRoles("USER")
+                //.authenticatedUserDetailsService(authenticationUserDetailsService())
+        ;
+
+//        http
+//                .formLogin()
+//                .loginPage("/login?reason=login")
+//                .failureUrl("/login?reason=failure")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .loginProcessingUrl("/login")
+//                .successForwardUrl("/secured/hei")
+//                .permitAll()
+//        ;
+//
+        http
+                .logout()
+                .logoutUrl("/log") // <== NOTE: Forutsetter http.csrf().disable() siden dette blir aktivert med GET!
+                .permitAll()
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/login?reason=logout")
+        ;
+
+        // Granted Authorities: ROLE_USER' stored to HttpSession:
+        http.httpBasic().realmName("MyRealmName");
 
         http
                 .authorizeRequests()
                 .antMatchers("/secured/**")
-                .hasAnyRole("USER", "DISTRIKT_LISTE", "DISTRIKT")
+                .hasAnyRole("USER") // <== "USER" funker men "ROLE_USER" fungerer ikke
                 .anyRequest()
                 .authenticated()
         ;
+
     }
+
 
 }
--- src/main/java/no/politiet/ft/DummyMvcController.java
+++ src/main/java/no/politiet/ft/DummyMvcController.java
@@ -4,9 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@EnableWebSecurity
+//@EnableWebSecurity
 @Controller
 @RequestMapping("/")
 public class DummyMvcController {
@@ -68,6 +66,19 @@ public class DummyMvcController {
 
     private StringBuilder appendStandardLoginForm(StringBuilder sb){
         return sb
+
+                .append("<form action='/login' method='post' >")
+                .append("<table><tr><td><label for='username'>Brukernavn</label></td>")
+                .append("<td><input type='text' id='username' name='username'/></td></tr>")
+                .append("<tr><td><label for='password'>Passord</label></td>")
+                .append("<td><input type='password' id='password' name='password'/></td></tr>")
+                .append("<tr><td colspan='2'>")
+                .append("<input type='submit' name='action' value='Logg inn'/>")
+                .append("</td></tr></table></form>")
+                ;
+    }
+    private StringBuilder oldappendStandardLoginForm(StringBuilder sb){
+        return sb
                 .append("<form action='j_security_check' method='post' >")
                 .append("<table><tr><td><label for='j_username'>Brukernavn</label></td>")
                 .append("<td><input type='text' id='j_username' name='j_username'/></td></tr>")
@@ -79,10 +90,11 @@ public class DummyMvcController {
                 ;
     }
 
+
     private StringBuilder generateUserFriendlyMessageFromReason(final String reason) {
         StringBuilder sb = new StringBuilder();
 
-        log.debug("Reason: >" + reason == null ? "<null>" : reason + "<");
+        log.debug("Reason: >" + (reason == null ? "<null>" : reason) + "<");
 
         if (reason != null && "logout".equals(reason)) {
             return sb // <== We do not want to have login capabilities, we want user to click to another page
